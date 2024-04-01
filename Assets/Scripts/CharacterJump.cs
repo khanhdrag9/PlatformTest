b8d31ac4f3426acdc4bfa5eb7afabb7fe490242c
khanhtn <khanhtn@bravestars.com>
2024-04-01 15:12:03 +0700

    added jump mechanics

--- Assets/Scripts/CharacterJump.cs
+++ Assets/Scripts/CharacterJump.cs
@@ -7,12 +7,24 @@ public class CharacterJump : MonoBehaviour
 {
     public float jumpHeight;
     public float timeToJumpApex;
-    public float gravityMultiplier;
+    public float jumpBuffer;
+    public float coyoteTime;
+    public float defaultGravityScale = 1;
+    public float upwardMovementMultiplier;
+    public float downwardMovementMultiplier;
+    public float jumpCutOff;
+    public float fallLimitSpeed;
 
 
     bool _isOnGround;
     bool _desiredJump;
     bool _pressingJump;
+    float _jumpBufferCounter;
+    float _coyoteTimeCounter;
+    bool _currentlyJumping;
+    Vector2 _velocity;
+    float _jumpSpeed;
+    float _gravityMultiplier = 1;
 
     Rigidbody2D _rb;
     PlayerGround _playerGround;
@@ -22,6 +34,7 @@ public class CharacterJump : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _playerGround = GetComponent<PlayerGround>();
+        _gravityMultiplier = defaultGravityScale;
     }
 
     // Update is called once per frame
@@ -33,18 +46,133 @@ public class CharacterJump : MonoBehaviour
 
     private void UpdatePhysics()
     {
-        var newGravity = new Vector2(0, (-2 * jumpHeight) / (timeToJumpApex * timeToJumpApex));
-        _rb.gravityScale = (newGravity.y / Physics2D.gravity.y) * gravityMultiplier;
+        // var newGravity = new Vector2(0, -2 * jumpHeight / (timeToJumpApex * timeToJumpApex));
+        // _rb.gravityScale = newGravity.y / Physics2D.gravity.y * _gravityMultiplier;
+
+        _isOnGround = _playerGround.isOnGround;
+
+        if (jumpBuffer > 0)
+        {
+            if (_desiredJump)
+            {
+                _jumpBufferCounter += Time.deltaTime;
+
+                if (_jumpBufferCounter > jumpBuffer)
+                {
+                    _desiredJump = false;
+                    _jumpBufferCounter = 0f;
+                }
+            }
+        }
+
+        if (!_currentlyJumping && !_isOnGround)
+        {
+            _coyoteTimeCounter += Time.deltaTime;
+        }
+        else
+        {
+            _coyoteTimeCounter = 0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _velocity = _rb.velocity;
+
+        if (_desiredJump)
+        {
+            // Do a jump
+            if (_isOnGround || _coyoteTimeCounter < coyoteTime && !_currentlyJumping)
+            {
+                _desiredJump = false;
+                _jumpBufferCounter = 0;
+                _coyoteTimeCounter = 0;
+
+                _jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * _rb.gravityScale * jumpHeight);
+
+                if (_velocity.y > 0f)
+                {
+                    _jumpSpeed = Mathf.Max(_jumpSpeed - _velocity.y, 0f);
+                }
+                else if (_velocity.y < 0f)
+                {
+                    _jumpSpeed += Mathf.Abs(_rb.velocity.y);
+                }
+
+                _velocity.y += _jumpSpeed;
+                _currentlyJumping = true;
+            }
+
+            if (jumpBuffer == 0)
+            {
+                _desiredJump = false;
+            }
+
+            _rb.velocity = _velocity;
+        }
+        else
+        {
+            // Calculate gravity
+            // Go up
+            if (_rb.velocity.y > 0.01f)
+            {
+                if (_isOnGround)
+                {
+                    _gravityMultiplier = defaultGravityScale;
+                }
+                else
+                {
+                    if (_pressingJump && _currentlyJumping)
+                    {
+                        _gravityMultiplier = upwardMovementMultiplier;
+                    }
+                    else
+                    {
+                        _gravityMultiplier = jumpCutOff;
+                    }
+                }
+            }
+            // Go down
+            else if (_rb.velocity.y < -0.01f)
+            {
+                if (_isOnGround)
+                {
+                    _gravityMultiplier = defaultGravityScale;
+                }
+                else
+                {
+                    _gravityMultiplier = downwardMovementMultiplier;
+                }
+            }
+            // not vertically move
+            else
+            {
+                if (_isOnGround)
+                {
+                    _currentlyJumping = false;
+                }
+
+                _gravityMultiplier = defaultGravityScale;
+            }
+        }
+
+        var newGravity = new Vector2(0, -2 * jumpHeight / (timeToJumpApex * timeToJumpApex));
+        _rb.gravityScale = newGravity.y / Physics2D.gravity.y * _gravityMultiplier;
+
+        _rb.velocity = new Vector2(_velocity.x, Mathf.Clamp(_velocity.y, -fallLimitSpeed, 100));
     }
 
     private void CheckInput()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetAxis("Jump") > 0)
         {
-            _pressingJump = true;
-            _desiredJump = true;
+            if (!_pressingJump)
+            {
+                _pressingJump = true;
+                _desiredJump = true;
+            }
         }
-        else if(Input.GetKeyUp(KeyCode.Space))
+        else if (Input.GetAxis("Jump") == 0)
         {
             _pressingJump = false;
         }
