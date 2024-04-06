9e1e53a5f4dd4522e6376e4bbc5ab386a7f2c327
khanhtranngoc <tkhanh783@gmail.com>
2024-04-06 12:54:48 +0700

    added key

--- Assets/Scripts/TeleportDoor.cs
+++ Assets/Scripts/TeleportDoor.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using LDtkUnity;
 using UnityEngine;
 
-public class TeleportDoor : MonoBehaviour
+public class TeleportDoor : MonoBehaviour, ILocker
 {
     public TeleportDoor toDoor;
     public Teleporter teleporter;
+    public GameObject lockedVisual;
+
+    public bool Lock { get; set; }
 
     private void Start()
     {
         var fields = GetComponent<LDtkFields>();
         toDoor = fields.GetEntityReference("teleport_to").FindEntity().GetComponent<TeleportDoor>();
+        Lock = fields.GetBool("is_locked");
     }
     private void Update()
     {
         teleporter.to = toDoor.teleporter;
+        teleporter.GetComponent<Collider2D>().enabled = !Lock && !toDoor.Lock;
+        lockedVisual.SetActive(Lock);
     }
 
 
     private void OnDrawGizmos()
     {
-        if (toDoor)
+        if (toDoor && !Lock)
         {
             Gizmos.color = Color.yellow;
             Gizmos.DrawLine(transform.position + Vector3.up * 0.5f, toDoor.transform.position + Vector3.up * 0.5f);
